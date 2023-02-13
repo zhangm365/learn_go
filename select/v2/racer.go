423f30a551b56a031fe3e650ef40f7a955e24289
zhangm365 <779253201@qq.com>
2023-02-13 15:34:20 +0800

    add files

--- select/v2/racer.go
+++ select/v2/racer.go
@@ -2,12 +2,12 @@ package racer
 
 import "net/http"
 
-func Racer(a, b string) (winner string) {
+func Racer(a, b string) (winner string, err error) {
 	select { // 同时在多个 channel 上等待返回信息，第一个发送值的 channel 「胜出」，case 代码执行
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
 	}
 }
 
--- select/v2/racer_test.go
+++ select/v2/racer_test.go
@@ -10,22 +10,39 @@ import (
 func TestRacer(t *testing.T) {
 	// slowURL := "https://www.google.com"
 	// fastURL := "https://www.baidu.com"
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0)
+	/*
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0)
 
-	// 函数结束时调用
-	defer slowServer.Close()
-	defer fastServer.Close()
+		// 函数结束时调用
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-	// fmt.Println(slowURL, fastURL)
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		// fmt.Println(slowURL, fastURL)
+		want := fastURL
+		got := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got '%s', want '%s'.", got, want)
-	}
+			if got != want {
+				t.Errorf("got '%s', want '%s'.", got, want)
+			}
+	*/
+	t.Run("return an error if a server doesn't respond within 10s.", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		// 函数结束时调用
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+
+	})
 
 }
 
