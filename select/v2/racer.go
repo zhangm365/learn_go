9b70f0848de18397da17fa96e510c7f4f782b496
zhangm365 <779253201@qq.com>
2023-02-20 22:43:47 +0800

    add files

--- select/v2/racer.go
+++ select/v2/racer.go
@@ -1,13 +1,20 @@
 package racer
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func Racer(a, b string) (winner string, err error) {
-	select { // 同时在多个 channel 上等待返回信息，第一个发送值的 channel 「胜出」，case 代码执行
+	select { // 同时在多个 channel 上等待返回信息，第一个发送值的 channel 「胜出」，对应的 case 代码执行
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
+	// 使用 select 时，time.After() 是一个很好的函数。
+	case <-time.After(10 * time.Second): // 超时时间是 10s。
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
